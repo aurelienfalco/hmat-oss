c3e1ec49e86cbc2f109e0e3858a6028161addc26
Aurélien Falco <aurelien.falco@inria.fr>
2016-11-15 18:07:56 +0100

    NULL assignments in cluster_tree

--- src/cluster_tree.cpp
+++ src/cluster_tree.cpp
@@ -152,8 +152,6 @@ ClusterTree::slice(int offset, int size) const
   ClusterTree* result = new ClusterTree(*this);
   result->data.offset_ = offset;
   result->data.size_ = size;
-  result->clusteringAlgoData_ = NULL;
-  result->admissibilityAlgoData_ = NULL;
   return result;
 }
 
@@ -242,4 +240,3 @@ AxisAlignedBoundingBox::distanceTo(const AxisAlignedBoundingBox& other) const
 }
 
 }  // end namespace hmat
-
